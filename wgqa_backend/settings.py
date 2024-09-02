59fa8a46766a43d3f7e7a6c56d51db2aa14919ba
Josh Lockhart <joshualockhart@gmail.com>
2024-09-02 22:59:19 +0000

    csrf

--- wgqa_backend/settings.py
+++ wgqa_backend/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware'
 ]
 
-CSRF_TRUSTED_ORIGINS = ['https://joshualockhart.pythonanywhere.com']
+CSRF_TRUSTED_ORIGINS = ['https://joshualockhart.pythonanywhere.com', "https://ec2-13-41-187-59.eu-west-2.compute.amazonaws.com"]
 
 CORS_ALLOWED_ORIGINS = [
     "https://localhost"
